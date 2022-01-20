88b357ab1a94e83f348548560d6fb01faf54a0c9
yota.ushizaki <nutsuray.027@gmail.com>
2022-01-20 13:17:11 +0000

    4th fixed

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizzbuzz(num)
     if num % 3 == 0 && num % 5 == 0
-        puts 'FizzBuzz'
+        'FizzBuzz'
     elsif num % 3 == 0
-        puts 'Fizz'
+        'Fizz'
     elsif num % 5 == 0
-        puts 'Buzz'
+        'Buzz'
     else
-        puts num
+        num
     end
 end
 
 num_max = 100   
 (1..num_max).each do |num|
-    fizzbuzz(num)
+    puts fizzbuzz(num)
 end
